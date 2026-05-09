b636e598f09ba93a559c44ca274811e8fd2bc8b2
cadebryant <cadehbryant@gmail.com>
2026-05-09 00:07:36 -0400

    Added activities and result models

---
+++ Theragraf.Functions/Activities/IngestionActivity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using OpenAI.Realtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Theragraf.Core.Models;
+
+namespace Theragraf.Functions.Activities
+{
+    public class IngestionActivity
+    {
+        [Function(nameof(IngestionActivity))]
+        public async Task<ObservationResult> Run([ActivityTrigger] TranscriptInput input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Theragraf.Functions/Activities/SoapActivity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Theragraf.Core.Models;
+
+namespace Theragraf.Functions.Activities
+{
+    public class SoapActivity
+    {
+        [Function(nameof(SoapActivity))]
+        public async Task<SoapNote> Run([ActivityTrigger] ObservationResult input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
