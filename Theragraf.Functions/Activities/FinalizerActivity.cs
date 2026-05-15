b8b150aed7dc18c3608a8b0782350183632e95e2
cadebryant <cadehbryant@gmail.com>
2026-05-14 23:42:15 -0400

    Added model and activity scaffolding for Finalizer

---
+++ Theragraf.Functions/Activities/FinalizerActivity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Theragraf.Core.Models;
+
+namespace Theragraf.Functions.Activities
+{
+    public class FinalizerActivity
+    {
+        [Function(nameof(FinalizerActivity))]
+        public async Task<FinalizeResult> Run([ActivityTrigger] ComplianceResult input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
